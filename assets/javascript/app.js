3f21e8cbfcbf3fd147066c99c402eb317897ee3e
Skip Khamvongsa <skipk95@gmail.com>
2020-03-03 21:47:26 -0700

    added style to css, fixed html, and added a image.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,9 +36,10 @@ var simpsonsTriv =
 //Variable that will hold our setInterval that runs the timer for 120 seconds
 var intervalId;
 //set our counter to 120
-var counter = 120;
+var counter = 50;
 var running = false;
 var choiceArray = [];
+
 //click to start
 $("#start").on("click", function() {
     $("#start").hide();
@@ -58,8 +59,9 @@ function decrement() {
     $("#timer-number").html("Time Remaining: " + counter);
     
     if (counter == 0) {
-        
+        checkAnswers();
         stop()
+        endResults();
     }
 }
 
@@ -71,18 +73,19 @@ function stop() {
 function display() {
     var displayQuest = $("#question-one");
     var answers = $(".answer-check");
+    
     displayQuest.append("<h2>These are the questions, Good luck, Have fun!</h2>");
     for (var i = 0; i < simpsonsTriv.length; i++) {
-        displayQuest.append('<div id="questions">' + simpsonsTriv[i].question + '</div>');
+        displayQuest.append('<div id="questions"><br><strong>' + simpsonsTriv[i].question + '</strong></br></div>');
 
         var choicesOne = simpsonsTriv[i].choices[0];
         var choicesTwo = simpsonsTriv[i].choices[1];
         var choicesThree = simpsonsTriv[i].choices[2];
         var choicesFour = simpsonsTriv[i].choices[3];
-        displayQuest.append(('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesOne + '</label></div>'));
-        displayQuest.append(('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesTwo + '</label></div>'));
-        displayQuest.append(('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesThree + '</label></div>'));
-        displayQuest.append(('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesFour + '</label></div>'));
+        displayQuest.append('<div class="answer-check"><input class="answerInput" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesOne + '</label></div>');
+        displayQuest.append('<div class="answer-check"><input class="answerInput" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesTwo + '</label></div>');
+        displayQuest.append('<div class="answer-check"><input class="answerInput" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesThree + '</label></div>');
+        displayQuest.append('<div class="answer-check"><input class="answerInput" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-checklabel" id="radio'+i+'label" for="radio'+i+'">' + choicesFour + '</label></div>');
     }
 }
 //Check if answers are correct, incorrect, or missed
@@ -92,10 +95,11 @@ function checkAnswers() {
     var incorrectNumber = 0;
     var unAns = 0;
     var userChoice;
-    for (var i = o; i < simpsonsTriv.length; i++) {
+    for (var i = 0; i < simpsonsTriv.length; i++) {
         correctAnswer = simpsonsTriv[i].answer;
-        userChoice = $('input[id=radio'+i+']:checked + label').text();
-
+        console.log(correctAnswer);
+        userChoice = $('form input[type=radio]:checked').text();
+        console.log(userChoice);
         if (userChoice === correctAnswer) {
             correctNumber++;
         }else if (userChoice === "") {
@@ -103,5 +107,16 @@ function checkAnswers() {
         } else if (userChoice !== correctAnswer) {
             incorrectNumber++;
         }
+        endResults(correctNumber, incorrectNumber, unAns);
     }
+}
+//after counter ends show hidden results page
+function endResults(correctNumber, incorrectNumber, unAns) {
+    $("#results").show();
+    $("#question-one").empty();
+    $("#timer-number").empty();
+    $("#here-text").text("Here are the results:");
+    $("#correct-score").text("Your correct answers!: " + correctNumber);
+    $("#incorrect-score").text("Wrong answers: " + incorrectNumber);
+    $("#unanswered-score").text("Unanswered questions: " + unAns);
 }
